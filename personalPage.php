233b6a8bd9cbfb4f84bb0303574c8a68124d5028
Luke <luca.mannella@studenti.polito.it>
2016-07-05 19:08:13 +0200

    La connessione viene chiusa solo se effettivamente aperta.

--- personalPage.php
+++ personalPage.php
@@ -30,28 +30,26 @@
                 </form>
 
     		<?php require_once './codePiece/noscript.php';	?>
-    		<?php if($loggedIn):
-
+    		<?php if($loggedIn) {
                 /**********************************/
-                /******* REMOVE RESERVATION *******/
+                /********* REMOVE PICTURES ********/
                 /**********************************/
-                if(count($_POST)!==0) {
-                    if(isset($_POST['DeleteAll'])) {
+                if (count($_POST) !== 0) {
+                    if (isset($_POST['DeleteAll'])) {
                         $conn = connectToDB($db_host, $db_user, $db_pass, $db_name);
 
-                        if($conn !== false)
-                        {
-                            $query = "SELECT MAC FROM customers WHERE email = '".$username."';";
-                                      
+                        if ($conn !== false) {
+                            $query = "SELECT MAC FROM customers WHERE email = '" . $username . "';";
+
                             $res = mysqli_query($conn, $query);
 
-                            if($res) {
+                            if ($res) {
                                 $row = mysqli_fetch_array($res);
-                                if($row != null) {
+                                if ($row != null) {
                                     $myMac = $row["MAC"];
                                     // in this case I suppose that for one email there is only one MAC
 
-                                    $query = "DELETE FROM suspicious_pictures WHERE MAC = '".$myMac."';";
+                                    $query = "DELETE FROM suspicious_pictures WHERE MAC = '" . $myMac . "';";
 
                                     $res = mysqli_query($conn, $query);
                                     if (!$res): ?>
@@ -59,10 +57,10 @@
                                             window.alert("Impossible to delete the pictures from the database!");
                                         </script>
                                     <?php else:
-                                        $myMac = str_replace("-","", $myMac);
-                                        $myPath = getcwd()."/suspicious_pictures/".$myMac."/";
-                                        $ok = DeleteDirectory($myPath);
-                                        if($ok) { ?>
+                                    $myMac = str_replace("-", "", $myMac);
+                                    $myPath = getcwd() . "/suspicious_pictures/" . $myMac . "/";
+                                    $ok = DeleteDirectory($myPath);
+                                    if ($ok) { ?>
                                         <script type="text/javascript">
                                             window.alert("Pictures successfully deleted!");
                                         </script>
@@ -73,45 +71,46 @@
                             mysqli_close($conn);
                         }
                     }
-	    		}
+                }
 
                 /***********************************/
                 /********* DISPLAY PICTURES ********/
                 /***********************************/
                 $conn = connectToDB($db_host, $db_user, $db_pass, $db_name);
-
-                $query = "SELECT Path
+                if ($conn != false) {
+                    $query = "SELECT Path
                           FROM suspicious_pictures SP, customers C
                           WHERE SP.MAC = C.MAC AND C.email = '$username';";
-                $res = mysqli_query($conn, $query);
-                if (!$res):
-                    echo "<p>Error during the download of the reservations!</p>";
-                else:
-                    $row = mysqli_fetch_array($res);
-                    if($row==NULL)
-                        echo "<BLOCKQUOTE><p><span class='darkgray'>There are no suspicious pictures right now.</span></h3></p></BLOCKQUOTE>";
-                    else {
-                        $counter = 1;
-                        while ($row != NULL) {
-                            $picture_path = $row["Path"];
-                            echo  "<img src='$picture_path' alt='picture$counter'>";
+                    $res = mysqli_query($conn, $query);
+                    if (!$res):
+                        echo "<p>Error during the download of the reservations!</p>";
+                    else:
+                        $row = mysqli_fetch_array($res);
+                        if ($row == NULL)
+                            echo "<BLOCKQUOTE><p><span class='darkgray'>There are no suspicious pictures right now.</span></h3></p></BLOCKQUOTE>";
+                        else {
+                            $counter = 1;
+                            while ($row != NULL) {
+                                $picture_path = $row["Path"];
+                                echo "<img src='$picture_path' alt='picture$counter'>";
 
-                            $row = mysqli_fetch_array($res);
-                            $counter++;
+                                $row = mysqli_fetch_array($res);
+                                $counter++;
+                            }
+                            if ($counter == 1)
+                                echo "<BLOCKQUOTE><p><span class='darkgray'>There are no suspicious pictures right now!</span></h3></p></BLOCKQUOTE>";
                         }
-                        if($counter == 1)
-                            echo "<BLOCKQUOTE><p><span class='darkgray'>There are no suspicious pictures right now!</span></h3></p></BLOCKQUOTE>";
-                    }
-                    mysqli_free_result($res);
-                endif;
+                        mysqli_free_result($res);
+                    endif;
 
-                mysqli_close($conn);
-
-      		else:
-      			echo "<h3>You can't see this page if you are not a registered user!</h3>";
-      			echo "<h4>Go <strong><a href='signUp.php'>here</a></strong> for create a new account!</h4>";
-      			echo "<h4>If you are already registered, please <strong><a href='login.php'>log in</a></strong>!</h4>";
-      		endif; ?>
+                    mysqli_close($conn);
+                }
+            }
+      		else {
+                echo "<h3>You can't see this page if you are not a registered user!</h3>";
+                echo "<h4>Go <strong><a href='signUp.php'>here</a></strong> for create a new account!</h4>";
+                echo "<h4>If you are already registered, please <strong><a href='login.php'>log in</a></strong>!</h4>";
+            } ?>
       		</div>
 		</div>
   		
